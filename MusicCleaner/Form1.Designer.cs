d354a4a062192791655d0991071f68626cb57968
dim4iksh <dim4iksh@gmail.com>
2012-01-30 14:43:21 +0200

    1 commit

---
+++ MusicCleaner/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MusicCleaner
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBrose = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.listBoxView = new System.Windows.Forms.ListBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonBrose
+            // 
+            this.buttonBrose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrose.Location = new System.Drawing.Point(538, 24);
+            this.buttonBrose.Name = "buttonBrose";
+            this.buttonBrose.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrose.TabIndex = 0;
+            this.buttonBrose.Text = "Brose";
+            this.buttonBrose.UseVisualStyleBackColor = true;
+            this.buttonBrose.Click += new System.EventHandler(this.buttonBrose_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(13, 33);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(83, 13);
+            this.labelPath.TabIndex = 1;
+            this.labelPath.Text = "Load the xml file";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Enabled = false;
+            this.textBoxPath.Location = new System.Drawing.Point(121, 25);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(351, 20);
+            this.textBoxPath.TabIndex = 2;
+            // 
+            // listBoxView
+            // 
+            this.listBoxView.FormattingEnabled = true;
+            this.listBoxView.Location = new System.Drawing.Point(16, 109);
+            this.listBoxView.Name = "listBoxView";
+            this.listBoxView.Size = new System.Drawing.Size(456, 134);
+            this.listBoxView.TabIndex = 3;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(538, 219);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "XML Files(*.xml)|*.xml";
+            this.openFileDialog1.InitialDirectory = "c:\\";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 270);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.listBoxView);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.buttonBrose);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "MusicCleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBrose;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ListBox listBoxView;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MusicCleaner/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MusicCleaner
+{
+    public partial class FormMain : Form
+    {
+        string fileName;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (DialogResult.OK == result)
+            {
+                fileName = openFileDialog1.FileName;
+                textBoxPath.Text = openFileDialog1.FileName;
+               
+            }
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
